9eac5c2f3ef60d97438318df8159561b563893ab
Software512 <163434801+Software512@users.noreply.github.com>
2025-06-30 16:33:24 -0500

    Better styling, added an optional header

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 var gameFile;
 var saveURL;
+var test;
 
 document.getElementById("gameUpload").addEventListener("input", (e) => {
     const file = e.target.files[0];
@@ -87,6 +88,7 @@ function error(message) {
 function getScreen() {
     const currentPathContents = getValueFromPath(gameFile.progress[gameFile.progress.length - 1]);
     document.getElementById("text").innerText = currentPathContents.text;
+    document.getElementById("header").textContent = currentPathContents.header;
     let optionsText = "Type ";
     if (currentPathContents.type == "end") {
         optionsText += "quit to exit the game or restart to start over."
@@ -135,23 +137,87 @@ function getScreen() {
         return;
     }
     if (style != undefined) {
-        document.getElementById("playScreen").style.backgroundColor = Object.hasOwn(style, "backgroundColor") ? style.backgroundColor : "";
-        document.getElementById("playScreen").style.color = Object.hasOwn(style, "color") ? style.color : "";
-        document.getElementById("playScreen").style.font = Object.hasOwn(style, "font") ? style.font : "";
-        document.getElementById("playScreen").style.fontSize = Object.hasOwn(style, "fontSize") ? style.fontSize : "";
-        document.getElementById("playScreen").style.fontStyle = Object.hasOwn(style, "fontStyle") ? style.fontStyle : "";
-        document.getElementById("playScreen").style.fontVariant = Object.hasOwn(style, "fontVariant") ? style.fontVariant : "";
-        document.getElementById("playScreen").style.fontWeight = Object.hasOwn(style, "fontWeight") ? style.fontWeight : "";
-        document.getElementById("playScreen").style.letterSpacing = Object.hasOwn(style, "letterSpacing") ? style.letterSpacing : "";
-        document.getElementById("playScreen").style.lineHeight = Object.hasOwn(style, "lineHeight") ? style.lineHeight : "";
-        document.getElementById("playScreen").style.textDecoration = Object.hasOwn(style, "textDecoration") ? style.textDecoration : "";
-        document.getElementById("playScreen").style.textDecorationColor = Object.hasOwn(style, "textDecorationColor") ? style.textDecorationColor : "";
-        document.getElementById("playScreen").style.textDecorationLine = Object.hasOwn(style, "textDecorationLine") ? style.textDecorationLine : "";
-        document.getElementById("playScreen").style.textDecorationStyle = Object.hasOwn(style, "textDecorationStyle") ? style.textDecorationStyle : "";
-        document.getElementById("playScreen").style.textDecorationThickness = Object.hasOwn(style, "textDecorationThickness") ? style.textDecorationThickness : "";
-        document.getElementById("playScreen").style.textShadow = Object.hasOwn(style, "textShadow") ? style.textShadow : "";
-        document.getElementById("playScreen").style.textTransform = Object.hasOwn(style, "textTransform") ? style.textTransform : "";
-        document.getElementById("playScreen").style.textUnderlineOffset = Object.hasOwn(style, "textUnderlineOffset") ? style.textUnderlineOffset : "";
+        // Very innefficient with code size but I'm too lazy.
+        if (Object.hasOwn(style, "all")) {
+            document.getElementById("playScreen").style.backgroundColor = Object.hasOwn(style.all, "backgroundColor") ? style.all.backgroundColor : "";
+            document.getElementById("playScreen").style.color = Object.hasOwn(style.all, "color") ? style.all.color : "";
+            document.getElementById("playScreen").style.font = Object.hasOwn(style.all, "font") ? style.all.font : "";
+            document.getElementById("playScreen").style.fontSize = Object.hasOwn(style.all, "fontSize") ? style.all.fontSize : "";
+            document.getElementById("playScreen").style.fontStyle = Object.hasOwn(style.all, "fontStyle") ? style.all.fontStyle : "";
+            document.getElementById("playScreen").style.fontVariant = Object.hasOwn(style.all, "fontVariant") ? style.all.fontVariant : "";
+            document.getElementById("playScreen").style.fontWeight = Object.hasOwn(style.all, "fontWeight") ? style.all.fontWeight : "";
+            document.getElementById("playScreen").style.letterSpacing = Object.hasOwn(style.all, "letterSpacing") ? style.all.letterSpacing : "";
+            document.getElementById("playScreen").style.lineHeight = Object.hasOwn(style.all, "lineHeight") ? style.all.lineHeight : "";
+            document.getElementById("playScreen").style.textDecorationColor = Object.hasOwn(style.all, "textDecorationColor") ? style.all.textDecorationColor : "";
+            document.getElementById("playScreen").style.textDecorationLine = Object.hasOwn(style.all, "textDecorationLine") ? style.all.textDecorationLine : "";
+            document.getElementById("playScreen").style.textDecorationStyle = Object.hasOwn(style.all, "textDecorationStyle") ? style.all.textDecorationStyle : "";
+            document.getElementById("playScreen").style.textDecorationThickness = Object.hasOwn(style.all, "textDecorationThickness") ? style.all.textDecorationThickness : "";
+            document.getElementById("playScreen").style.textShadow = Object.hasOwn(style.all, "textShadow") ? style.all.textShadow : "";
+            document.getElementById("playScreen").style.textTransform = Object.hasOwn(style.all, "textTransform") ? style.all.textTransform : "";
+            document.getElementById("playScreen").style.textUnderlineOffset = Object.hasOwn(style.all, "textUnderlineOffset") ? style.all.textUnderlineOffset : "";
+        } else {
+            document.getElementById("playScreen").style = "";
+        }
+        if (Object.hasOwn(style, "header")) {
+            document.getElementById("header").style.backgroundColor = Object.hasOwn(style.header, "backgroundColor") ? style.header.backgroundColor : "";
+            document.getElementById("header").style.color = Object.hasOwn(style.header, "color") ? style.header.color : "";
+            document.getElementById("header").style.font = Object.hasOwn(style.header, "font") ? style.header.font : "";
+            document.getElementById("header").style.fontSize = Object.hasOwn(style.header, "fontSize") ? style.header.fontSize : "";
+            document.getElementById("header").style.fontStyle = Object.hasOwn(style.header, "fontStyle") ? style.header.fontStyle : "";
+            document.getElementById("header").style.fontVariant = Object.hasOwn(style.header, "fontVariant") ? style.header.fontVariant : "";
+            document.getElementById("header").style.fontWeight = Object.hasOwn(style.header, "fontWeight") ? style.header.fontWeight : "";
+            document.getElementById("header").style.letterSpacing = Object.hasOwn(style.header, "letterSpacing") ? style.header.letterSpacing : "";
+            document.getElementById("header").style.lineHeight = Object.hasOwn(style.header, "lineHeight") ? style.header.lineHeight : "";
+            document.getElementById("header").style.textDecorationColor = Object.hasOwn(style.header, "textDecorationColor") ? style.header.textDecorationColor : "";
+            document.getElementById("header").style.textDecorationLine = Object.hasOwn(style.header, "textDecorationLine") ? style.header.textDecorationLine : "";
+            document.getElementById("header").style.textDecorationStyle = Object.hasOwn(style.header, "textDecorationStyle") ? style.header.textDecorationStyle : "";
+            document.getElementById("header").style.textDecorationThickness = Object.hasOwn(style.header, "textDecorationThickness") ? style.header.textDecorationThickness : "";
+            document.getElementById("header").style.textShadow = Object.hasOwn(style.header, "textShadow") ? style.header.textShadow : "";
+            document.getElementById("header").style.textTransform = Object.hasOwn(style.header, "textTransform") ? style.header.textTransform : "";
+            document.getElementById("header").style.textUnderlineOffset = Object.hasOwn(style.header, "textUnderlineOffset") ? style.header.textUnderlineOffset : "";
+        } else {
+            document.getElementById("header").style = "";
+        }
+        if (Object.hasOwn(style, "text")) {
+            document.getElementById("text").style.backgroundColor = Object.hasOwn(style.text, "backgroundColor") ? style.text.backgroundColor : "";
+            document.getElementById("text").style.color = Object.hasOwn(style.text, "color") ? style.text.color : "";
+            document.getElementById("text").style.font = Object.hasOwn(style.text, "font") ? style.text.font : "";
+            document.getElementById("text").style.fontSize = Object.hasOwn(style.text, "fontSize") ? style.text.fontSize : "";
+            document.getElementById("text").style.fontStyle = Object.hasOwn(style.text, "fontStyle") ? style.text.fontStyle : "";
+            document.getElementById("text").style.fontVariant = Object.hasOwn(style.text, "fontVariant") ? style.text.fontVariant : "";
+            document.getElementById("text").style.fontWeight = Object.hasOwn(style.text, "fontWeight") ? style.text.fontWeight : "";
+            document.getElementById("text").style.letterSpacing = Object.hasOwn(style.text, "letterSpacing") ? style.text.letterSpacing : "";
+            document.getElementById("text").style.lineHeight = Object.hasOwn(style.text, "lineHeight") ? style.text.lineHeight : "";
+            document.getElementById("text").style.textDecorationColor = Object.hasOwn(style.text, "textDecorationColor") ? style.text.textDecorationColor : "";
+            document.getElementById("text").style.textDecorationLine = Object.hasOwn(style.text, "textDecorationLine") ? style.text.textDecorationLine : "";
+            document.getElementById("text").style.textDecorationStyle = Object.hasOwn(style.text, "textDecorationStyle") ? style.text.textDecorationStyle : "";
+            document.getElementById("text").style.textDecorationThickness = Object.hasOwn(style.text, "textDecorationThickness") ? style.text.textDecorationThickness : "";
+            document.getElementById("text").style.textShadow = Object.hasOwn(style.text, "textShadow") ? style.text.textShadow : "";
+            document.getElementById("text").style.textTransform = Object.hasOwn(style.text, "textTransform") ? style.text.textTransform : "";
+            document.getElementById("text").style.textUnderlineOffset = Object.hasOwn(style.text, "textUnderlineOffset") ? style.text.textUnderlineOffset : "";
+        } else {
+            document.getElementById("text").style = "";
+        }
+        if (Object.hasOwn(style, "choices")) {
+            document.getElementById("inputOptions").style.backgroundColor = Object.hasOwn(style.choices, "backgroundColor") ? style.choices.backgroundColor : "";
+            document.getElementById("inputOptions").style.color = Object.hasOwn(style.choices, "color") ? style.choices.color : "";
+            document.getElementById("inputOptions").style.font = Object.hasOwn(style.choices, "font") ? style.choices.font : "";
+            document.getElementById("inputOptions").style.fontSize = Object.hasOwn(style.choices, "fontSize") ? style.choices.fontSize : "";
+            document.getElementById("inputOptions").style.fontStyle = Object.hasOwn(style.choices, "fontStyle") ? style.choices.fontStyle : "";
+            document.getElementById("inputOptions").style.fontVariant = Object.hasOwn(style.choices, "fontVariant") ? style.choices.fontVariant : "";
+            document.getElementById("inputOptions").style.fontWeight = Object.hasOwn(style.choices, "fontWeight") ? style.choices.fontWeight : "";
+            document.getElementById("inputOptions").style.letterSpacing = Object.hasOwn(style.choices, "letterSpacing") ? style.choices.letterSpacing : "";
+            document.getElementById("inputOptions").style.lineHeight = Object.hasOwn(style.choices, "lineHeight") ? style.choices.lineHeight : "";
+            document.getElementById("inputOptions").style.textDecorationColor = Object.hasOwn(style.choices, "textDecorationColor") ? style.choices.textDecorationColor : "";
+            document.getElementById("inputOptions").style.textDecorationLine = Object.hasOwn(style.choices, "textDecorationLine") ? style.choices.textDecorationLine : "";
+            document.getElementById("inputOptions").style.textDecorationStyle = Object.hasOwn(style.choices, "textDecorationStyle") ? style.choices.textDecorationStyle : "";
+            document.getElementById("inputOptions").style.textDecorationThickness = Object.hasOwn(style.choices, "textDecorationThickness") ? style.choices.textDecorationThickness : "";
+            document.getElementById("inputOptions").style.textShadow = Object.hasOwn(style.choices, "textShadow") ? style.choices.textShadow : "";
+            document.getElementById("inputOptions").style.textTransform = Object.hasOwn(style.choices, "textTransform") ? style.choices.textTransform : "";
+            document.getElementById("inputOptions").style.textUnderlineOffset = Object.hasOwn(style.choices, "textUnderlineOffset") ? style.choices.textUnderlineOffset : "";
+        } else {
+            document.getElementById("inputOptions").style = "";
+        }
     }
 }
 
